70f5bdf8fc2d8e38feb16294dae312e96c024e60
Charles Bourgeaux <ch.bourgeaux@gmail.com>
2020-12-14 11:37:16 +0100

    feat: externalized configuration file

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Uguu-client
+
+[![GitHub license](https://img.shields.io/github/license/charlyie/uguu-client.svg)](https://github.com/charlyie/uguu-client/blob/master/LICENSE) [![GitHub release](https://img.shields.io/github/v/tag/charlyie/uguu-client.svg)](https://GitHub.com/charlyie/uguu-client/releases/) [![GitHub issues](https://img.shields.io/github/issues/charlyie/uguu-client.svg)](https://GitHub.com/charlyie/uguu-client/issues/)
+
 Uguu-compatible command line bash client
 This client is able to send local files fo [Uguu](https://github.com/nokonoko/Uguu) compatible client like [this fork](https://github.com/charlyie/Uguu).
+
+![Uguu Bash Client](https://i.ibb.co/HzKm41p/cli-screenshot.jpg)
+
 Uguu is a simple lightweight temporary file uploading and sharing platform where files get deleted after X amount of time.
 
 ## Features
 
 - Easy server file uploading, no registration required
+- Custom Endpoint Management
 
 
 ## Requirements
@@ -14,7 +21,18 @@ Requires packages `curl` and `jq` installed on the Operating System.
 
 ## Install
 
+### Quick method (for Debian/Ubuntu)
+
+```bash
+$ apt-get install jq curl -y -q;wget -q https://raw.githubusercontent.com/charlyie/uguu-client/main/ushare.sh -O /usr/sbin/ushare;chmod +x /usr/sbin/ushare;ushare -v
+```
+
+
+
+### Git method
+
 First you must get a copy of the uguu-clent code.  To do so, clone this git repo.
+
 ```bash
 git clone https://github.com/charlyie/uguu-client.git
 ```
@@ -27,27 +45,63 @@ chmod +x ushare.sh
 cp ushare.sh /usr/bin/ushare
 ```
 
+
+
 ## Usage
 
 ```bash
-ushare myfile.zip
+$ ushare myfile.sql.gz 
+[Uguu] Starting transferring file myfile.sql.gz (426M)...
+######################################################################## 100.0%
+[Uguu] Link to share : https://s.aice.am/unbgthXcGe0p.sql.gz
 ```
 The app returns the URL provided by the API.
 
 
+
 ## Change Uguu's endpoint 
 
-By default, the client uses the main API client : https://uguu.se/, but this url can be changed by editing the .sh file :
+By default, the client uses the main API client : https://uguu.se/. This endpoint can be changed by using the `--endpoint` argument, like : 
 ```bash
-# Editable variables
-ENDPOINT="https://uguu.se"
+$ ushare --endpoint "https://share.maecia.com"
+[Uguu] Configuration saved in /etc/ushare.conf
 ```
+
+
+
+### Check Configuration
+
+An overrided configuration is stored in `/etc/ushare.conf`. The configuration loaded can be verified by using the `--info`argument, like :
+
+```bash
+$ ushare --info
+Configuration loaded:
+> App Name :             Uguu
+> Version :              1.1.0
+> Configuration File :   /etc/ushare.conf
+> Endpoint :             https://share.maecia.com
+```
+
+### 
+
+## Changelog
+
+- **1.1.0 (build 14122020)** :
+  - Added custom configuration file
+  - Added method to set/get endpoint
+  - New Installation method
+  - Code cleaning
+- **1.0.0 (build 04122020)** : 
+  - initial release
+
 
 
 ## Credits
 
 Uguu client was developed by [Charles Bourgeaux](https://github.com/charlyie)
 Uguu is based on [Pomf](http://github.com/pomf/pomf).
+
+
 
 ## License
 
--- ushare.sh
+++ ushare.sh
@@ -13,21 +13,50 @@ ENDPOINT="https://uguu.se"
 APPNAME="Uguu"
 # End of editable variables
 
-APP_VERSION="1.0.0"
+APP_VERSION="1.1.0"
+OVERRIDE_CONF_FILE="/etc/ushare.conf"
 GREEN="\033[0;32m"
 WHITE="\033[1;37m"
 NC="\033[0m" # No Color
 REQUIRED_PACKAGES=( "curl" "jq" )
 
+#Load Configuration file if provided
+if [ -f "$OVERRIDE_CONF_FILE" ]; then
+    source "$OVERRIDE_CONF_FILE"
+fi
+
 # Output printing function
 cli_output(){
 	if [[ $2 == "nohead" ]]; 
 	then 
-		printf "$1\n"
+		printf "${1}\n"
 	else
-		printf "${WHITE}[${GREEN}${APPNAME}${WHITE}] $1${NC}\n"
+		printf "${WHITE}[${GREEN}${APPNAME}${WHITE}] %s${NC}\n" "$1"
 	fi
 }
+
+# Display Current Configuration
+get_configuration(){
+    #Load Configuration file if provided
+    if [ ! -f "$OVERRIDE_CONF_FILE" ]; then
+        OVERRIDE_CONF_FILE="None"
+    fi
+    cli_output "Configuration loaded:" nohead
+    cli_output "> App Name :\t\t ${APPNAME}" nohead
+    cli_output "> Version :\t\t ${APP_VERSION}" nohead
+    cli_output "> Configuration File :\t ${OVERRIDE_CONF_FILE}" nohead
+    cli_output "> Endpoint :\t\t ${ENDPOINT}" nohead
+}
+
+# Set a Configuration
+set_configuration(){
+    cat >$OVERRIDE_CONF_FILE <<EOL
+ENDPOINT="$ENDPOINT"
+APPNAME="$APPNAME"
+EOL
+    cli_output "Configuration saved in ${OVERRIDE_CONF_FILE}"
+}
+
 
 # Manage arguments
 while [[ $# -gt 0 ]]
@@ -36,18 +65,32 @@ key="$1"
 
 case $key in
     -h|--help)
-    shift # past argument
     cli_output "UGUU client v.${APP_VERSION}, a Command Line Interface for UGUU compatible API" nohead
 	cli_output "(c) Charles Bourgeaux <charles@resmush.it>\n" nohead
 	cli_output "Usage: ushare <filename>"  nohead
 	cli_output "Startup:" nohead
-	cli_output "  -h or --help \t\t\t\t\t print this help." nohead
-	cli_output "  -v or --version \t\t\t\t display the version of this client." nohead
+    cli_output "  -e <endpoint> or --endpoint <endpoint> \t\t set a new endpoint." nohead
+    cli_output "  -h or --help \t\t\t\t\t print this help." nohead
+	cli_output "  -i or --info \t\t display configuration informations." nohead
+    cli_output "  -v or --version \t\t\t\t display the version of this client." nohead
+    shift # past argument
 	exit 0
     ;;
     -v|--version)
-    shift # past argument
     cli_output "UGUU client v.${APP_VERSION}" nohead
+    shift # past argument
+    exit 0
+    ;;
+    -i|--info)
+    get_configuration
+    shift # past argument
+    exit 0
+    ;;
+    -e|--endpoint)
+    ENDPOINT="$2"
+    set_configuration
+    shift # past argument
+    shift # past argument
     exit 0
     ;;
     -*)    # unknown option
@@ -68,9 +111,9 @@ esac
 done
 
 # Package requirements check
-for x in ${REQUIRED_PACKAGES[@]}
+for x in "${REQUIRED_PACKAGES[@]}"
 do
-if ! which $x > /dev/null; 
+if ! which "$x" > /dev/null; 
 then
   cli_output "The command $x was not found on your system. Try to install it before running this app!"
   exit 0
@@ -84,14 +127,14 @@ if [ -z "$FILE" ]; then
 fi
 
 # File provided available ?
-FILENAME="$(basename -- $FILE)"
+FILENAME=$(basename -- "$FILE")
 if [ ! -f "$FILE" ]; then
     cli_output "File ${FILE} was not found!"
     exit 1
 fi
 
 # Call the API
-FILESIZE=$(du -h ${FILE} | awk '{print $1}')
+FILESIZE=$(du -h "${FILE}" | awk '{print $1}')
 cli_output "Starting transferring file ${FILENAME} (${FILESIZE})..."
 
 RESPONSE=$(curl -# -F "files[]=@${FILE}" "${ENDPOINT}/upload.php")
